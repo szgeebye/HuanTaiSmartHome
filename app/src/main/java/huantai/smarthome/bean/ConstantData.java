3ae32be6f8aaa8e4004cb66818d50f769adb6330
luojie <luojie199403@126.com>
2017-09-08 11:51:41 +0800

    常量数据封装

---
+++ app/src/main/java/huantai/smarthome/bean/ConstantData.java
@@ -0,0 +1,16 @@
+package huantai.smarthome.bean;
+
+/**
+ * Auther：luojie
+ * E-mail：luojie@hnu.edu.cn
+ * Time：2017/9/8 11:39
+ */
+public class ConstantData {
+          public static final String name[]={
+               "温度","湿度","烟雾","燃气","报警","门","人体感应","LED灯","空调"
+          };
+          public static final String key[]={
+               "Temperature","Humidity","smoke1","gas1","Alert_1","gate1","body1","LED_OnOff","Send_com"
+          };
+
+}
